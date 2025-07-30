836cec4e1683109289a0bb8bbd29011ceda356ad
SMITPak <smit_web_and_app@gmail.com>
2025-07-30 22:28:31 +0500

    add tawk to script

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    // Tawk.to script
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://embed.tawk.to/688a5591eb270d19290af5aa/1j1e5sgfu";
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return <div>Hello World Testing</div>;
 };
 
